75f750ee4d13d91736766ddc7d79dbcad56a2bd7
fuh783 <fuh783@13322.com>
2018-03-29 12:14:25 +0800

    v1.0

--- jquery/lib/template/template.js
+++ jquery/lib/template/template.js
@@ -5,17 +5,55 @@
 	//开始使用
 	let install = require("./install.art");
 	let intro = require("./intro.art");
-	let standard = require("./standard.art");
 	
 	//基础组件
 	let button = require("./button.art");
-
+	let input = require("./input.art");
+	let list = require("./list.art");
+	let slider = require("./slider.art");
+	let uploader = require("./uploader.art");
+	let badge = require("./badge.art");
+	let flex = require("./flex.art");
+	let gallery = require("./gallery.art");
+	let grid = require("./grid.art");
+	let icon = require("./icon.art");
+	let loadmore = require("./loadmore.art");
+	let progress = require("./progress.art");
+	
+	
+	let actionsheet = require("./actionsheet.art");
+	let dialog = require("./dialog.art");
+	let msg = require("./msg.art");
+	let picker = require("./picker.art");
+	let toast = require("./toast.art");
+	let navbar = require("./navbar.art");
+	let tabbar = require("./tabbar.art");
+	let searchbar = require("./searchbar.art");
+	
 	let template = {
 		"install": install({})	
 		,"intro": intro({})	
-		,"standard": standard({})	
 		,"button": button({})
-			
+		,"input": input({})
+		,"list": list({})
+		,"slider": slider({})
+		,"uploader": uploader({})
+		,"badge": badge({})
+		,"flex": flex({})
+		,"gallery": gallery({})
+		,"grid": grid({})
+		,"icon": icon({})
+		,"loadmore": loadmore({})
+		,"progress": progress({})
+		
+		,"actionsheet": actionsheet({})
+		,"dialog": dialog({})
+		,"msg": msg({})
+		,"picker": picker({})
+		,"toast": toast({})
+		,"navbar": navbar({})
+		,"tabbar": tabbar({})
+		,"searchbar": searchbar({})
 	};
 	
 	window.template = template;
